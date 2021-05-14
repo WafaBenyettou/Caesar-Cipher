ab859178ea2e7ff6e08dde2a37a73fae9152499a
Wafa <83190429+WafaBenyettou@users.noreply.github.com>
2021-05-15 00:37:05 +0200

    Create CCC.java

---
+++ CCC.java
@@ -0,0 +1,234 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+public class CCC {
+    public static void main (String[]args){
+        Scanner input= new Scanner(System.in);
+        System.out.println("enter your phrase ! ");
+        String str= input.nextLine();
+        for ( int i=0; i<str.length(); i++){
+            char x = str.charAt(i);
+            switch ( x )
+            {
+                case ' ':
+                     System.out.print(" ");
+                     break;
+    
+                     case 'A':
+                     System.out.print("D");
+                     break;
+                     case 'B':
+                     System.out.print("E");
+                     break;
+                     case 'C':
+                     System.out.print("F");
+                     break;
+                     case 'D':
+                     System.out.print("G");
+                     break;
+                     case 'E':
+                     System.out.print("H");
+                     break;
+                     case 'F':
+                     System.out.print("I");
+                     break;
+                     case 'G':
+                     System.out.print("J");
+                     break;
+                     case 'H':
+                     System.out.print("K");
+                     break;
+                     case 'I':
+                     System.out.print("L");
+                     break;
+                     case 'J':
+                     System.out.print("M");
+                     break;
+                     case 'K':
+                     System.out.print("N");
+                     break;
+                     case 'L':
+                     System.out.print("O");
+                     break;
+                     case 'M':
+                     System.out.print("P");
+                     break;
+                     case 'N':
+                     System.out.print("Q");
+                     break;
+                     case 'O':
+                     System.out.print("R");
+                     break;
+                     case 'P':
+                     System.out.print("S");
+                     break;
+                     case 'Q':
+                     System.out.print("T");
+                     break;
+                     case 'R':
+                     System.out.print("U");
+                     break;
+                     case 'S':
+                     System.out.print("V");
+                     break;
+                     case 'T':
+                     System.out.print("W");
+                     break;
+                     case 'U':
+                     System.out.print("X");
+                     break;
+                     case 'V':
+                     System.out.print("Y");
+                     break;
+                     case 'W':
+                     System.out.print("Z");
+                     break;
+                     case 'X':
+                     System.out.print("A");
+                     break;
+                     case 'Y':
+                     System.out.print("B");
+                     break;
+                     case 'Z':
+                     System.out.print("C");
+                     break;
+                     case 'a':
+                     System.out.print("d");
+                     break;
+                     case 'b':
+                     System.out.print("e");
+                     break;
+                     case 'c':
+                     System.out.print("f");
+                     break;
+                     case 'd':
+                     System.out.print("g");
+                     break;
+                     case 'e':
+                     System.out.print("h");
+                     break;
+                     case 'f':
+                     System.out.print("i");
+                     break;
+                     case 'g':
+                     System.out.print("j");
+                     break;
+                     case 'h':
+                     System.out.print("k");
+                     break;
+                     case 'i':
+                     System.out.print("l");
+                     break;
+                     case 'j':
+                     System.out.print("m");
+                     break;
+                     case 'k':
+                     System.out.print("n");
+                     break;
+                     case 'l':
+                     System.out.print("o");
+                     break;
+                     case 'm':
+                     System.out.print("p");
+                     break;
+                     case 'n':
+                     System.out.print("q");
+                     break;
+                     case 'o':
+                     System.out.print("r");
+                     break;
+                     case 'p':
+                     System.out.print("s");
+                     break;
+                     case 'q':
+                     System.out.print("t");
+                     break;
+                     case 'r':
+                     System.out.print("u");
+                     break;
+                     case 's':
+                     System.out.print("v");
+                     break;
+                     case 't':
+                     System.out.print("w");
+                     break;
+                     case 'u':
+                     System.out.print("x");
+                     break;
+                     case 'v':
+                     System.out.print("y");
+                     break;
+                     case 'w':
+                     System.out.print("z");
+                     break;
+                     case 'x':
+                     System.out.print("a");
+                     break;
+                     case 'y':
+                     System.out.print("b");
+                     break;
+                     case 'z':
+                     System.out.print("c");
+                     break;
+                        case '.':
+                     System.out.print(".");
+                     break;
+                      case ',':
+                     System.out.print(",");
+                     break;
+                      case '?':
+                     System.out.print("?");
+                     break;
+                     case '!':
+                     System.out.print("!");
+                     break;
+                      case '/':
+                     System.out.print("/");
+                     break;
+                      case '@':
+                     System.out.print("@");
+                     break;
+                      case '0':
+                     System.out.print("0");
+                     break;
+                      case '1':
+                     System.out.print("1");
+                     break;
+                      case '2':
+                     System.out.print("2");
+                     break;
+                      case '3':
+                     System.out.print("3");
+                     break;
+                      case '4':
+                     System.out.print("4");
+                     break;
+                      case '5':
+                     System.out.print("5");
+                     break;
+                      case '6':
+                     System.out.print("6");
+                     break;
+                      case '7':
+                     System.out.print("7");
+                     break;
+                      case '8':
+                     System.out.print("8");
+                     break;
+                      case '9':
+                     System.out.print("9");
+                     break;
+                      default : 
+                          System.out.print("unvalide");
+                     break;
+                          
+                    
+                      
+                    
+            }
+        }
+    }
+    
+}
